b4c22d9477bc46eeb0de7d1ea51e631bd5810146
Schezuk <7524886+Schezuk@users.noreply.github.com>
2022-06-17 21:15:37 +0800

    Update comic.dart

--- lib/app/api/comic.dart
+++ lib/app/api/comic.dart
@@ -41,12 +41,78 @@ class ComicApi {
     var resultBytes = ApiUtil.decrypt(result);
 
     var data = ComicDetailResponse.fromBuffer(resultBytes);
-    if (data.errno != 0) {
-      throw AppError(data.errmsg, code: data.errno);
-    }
+
+  
+    //patch start
+    if (data.errno == 0) {
     return data.data;
+    }
+    //fallback
+    try{
+    path = "https://api.dmzj.com/dynamic/comicinfo/$comicId.json";
+    var info = await HttpUtil.instance.httpGet(
+      path,
+      queryParameters: ApiUtil.defaultParameter(needLogined: true),
+    );
+    var ret = {
+      "Data": {
+        "Id": info['data']['info']['id'],
+        "Title": info['data']['info']['title'],
+        "Direction": info['data']['info']['direction'],
+        "Islong": info['data']['info']['islong'],
+        "Cover": info['data']['info']['cover'],
+        "Description": info['data']['info']['description'],
+        "LastUpdatetime": info['data']['info']['last_updatetime'],
+        "LastUpdateChapterName": info['data']['info']['last_update_chapter_name'],
+        "FirstLetter": info['data']['info']['first_letter'],
+        "ComicPy": info['data']['info']['first_letter'],
+        "HotNum": 0,//N/A
+        "HitNum": 0,//N/A
+        "LastUpdateChapterId": info['data']['list'][0]['id'],
+        "Types": [
+          {
+            "TagId": 0,//WIP
+            "TagName": info['data']['info']['types'],
+          }
+        ],
+        "Status": [
+          {
+            "TagId": info['data']['info']['status']=="连载中"?2309:2310,
+            "TagName": info['data']['info']['status']
+          }
+        ],
+        "Authors": [
+          {
+            "TagId": 0,//WIP
+            "TagName": info['data']['info']['status']
+          }
+        ],
+        "SubscribeNum": 0,//N/A
+        "Chapters": [
+          {
+            "Title": "连载",//N/A
+            "Data": info['data']['list'].map((e){
+              return {
+                "ChapterId": e['id'],
+                "ChapterTitle": e['chapter_name'],
+                "Updatetime": e['updatetime'],
+                "Filesize": e['filesize'],
+                "ChapterOrder": e['chapter_order']
+              };
+            }),
+          }
+        ],
+        "IsNeedLogin": 1//N/A
+      }
+    };
+    }catch(e){
+      throw AppError(data.errmsg, code: data.errno);//if v1 fails, return v4 error
+    }
+    //patch end
   }
 
+  
+  
   /// 首页-排行榜
   Future<List<ComicRankListItemResponse>> getRankList(
       {int tagId = 0, int byTime = 0, int rankType, int page = 0}) async {
