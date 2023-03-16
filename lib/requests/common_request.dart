9a7e32331aad757e54c175f70f401a3d02e4e320
xiaoyaocz <xiaoyaocz@52uwp.com>
2023-03-16 09:23:30 +0800

    修改检查更新链接

--- lib/requests/common_request.dart
+++ lib/requests/common_request.dart
@@ -3,8 +3,30 @@ import 'package:flutter_dmzj/models/version_model.dart';
 
 /// 通用的请求
 class CommonRequest {
-  /// 检查更新
   Future<VersionModel> checkUpdate() async {
+    try {
+      return await checkUpdateGitMirror();
+    } catch (e) {
+      return await checkUpdateJsDelivr();
+    }
+  }
+
+  /// 检查更新
+  Future<VersionModel> checkUpdateGitMirror() async {
+    var result = await Dio().get(
+      "https://raw.gitmirror.com/xiaoyaocz/flutter_dmzj/main/document/app_version.json",
+      queryParameters: {
+        "ts": DateTime.now().millisecondsSinceEpoch,
+      },
+      options: Options(
+        responseType: ResponseType.json,
+      ),
+    );
+    return VersionModel.fromJson(result.data);
+  }
+
+  /// 检查更新
+  Future<VersionModel> checkUpdateJsDelivr() async {
     var result = await Dio().get(
       "https://cdn.jsdelivr.net/gh/xiaoyaocz/flutter_dmzj@main/document/app_version.json",
       queryParameters: {
